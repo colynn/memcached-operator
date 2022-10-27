1627bca5b08d749b2068c3c9da6710bc2bac26f5
colynn.liu <colynn.liu@gmail.com>
2022-10-27 20:46:30 +0800

    ✨ feat: add real validate webhook

--- api/v1alpha1/memcached_webhook.go
+++ api/v1alpha1/memcached_webhook.go
@@ -17,6 +17,8 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"errors"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -42,7 +44,9 @@ var _ webhook.Defaulter = &Memcached{}
 func (r *Memcached) Default() {
 	memcachedlog.Info("default", "name", r.Name)
 
-	// TODO(user): fill in your defaulting logic.
+	if r.Spec.Size == 0 {
+		r.Spec.Size = 3
+	}
 }
 
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
@@ -54,16 +58,14 @@ var _ webhook.Validator = &Memcached{}
 func (r *Memcached) ValidateCreate() error {
 	memcachedlog.Info("validate create", "name", r.Name)
 
-	// TODO(user): fill in your validation logic upon object creation.
-	return nil
+	return validateOdd(r.Spec.Size)
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *Memcached) ValidateUpdate(old runtime.Object) error {
 	memcachedlog.Info("validate update", "name", r.Name)
 
-	// TODO(user): fill in your validation logic upon object update.
-	return nil
+	return validateOdd(r.Spec.Size)
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
@@ -71,5 +73,12 @@ func (r *Memcached) ValidateDelete() error {
 	memcachedlog.Info("validate delete", "name", r.Name)
 
 	// TODO(user): fill in your validation logic upon object deletion.
+	return nil
+}
+
+func validateOdd(n int32) error {
+	if n%2 == 0 {
+		return errors.New("cluster size must be an odd number")
+	}
 	return nil
 }
